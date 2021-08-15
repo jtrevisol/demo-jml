8628b283aa30f5adfd70bbc0f094adab91b7c86b
jonas <jtrevisol@gmail.com>
2021-08-15 02:02:20 -0300

    primeiro commit

---
+++ src/main/java/com/example/demojml/DemoJmlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demojml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJmlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demojml/DemoJmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demojml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
